53381c2b076a57a17d910d735c48e58e855d836a
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-02-04 12:21:06 +0600

    add new config in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,29 @@
 /** @type {import('next').NextConfig} */
+// const withCSS = require('@zeit/next-css');
+// const compose = require('next-compose');
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
       },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@acme/ui'],
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/chunks/[path][name].[hash][ext]",
+      },
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
